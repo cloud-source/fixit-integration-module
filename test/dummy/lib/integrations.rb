55db1c1b05a7aeac28903b42deaf7d908f7b1b61
Rafael Santos <formigarafa@gmail.com>
2017-07-21 00:32:40 +1200

    mock a few more integrations methods

--- test/dummy/lib/integrations.rb
+++ test/dummy/lib/integrations.rb
@@ -10,14 +10,52 @@ module Integrations
     channel
   end
 
-  def self.update_user(user_id, data)
-    @users ||= {}
+  def create_user(data)
+    @users ||= []
     @last_user_id ||= 0
-    user = @users[user_id] ||= OpenStruct.new(id: user_id, created_at: Time.current)
+    user = OpenStruct.new(id: (@last_user_id += 1), created_at: Time.current)
+    data.each do |k, v|
+      user[k] = v
+    end
+    @users.push user
+    user
+  end
+
+  def self.update_user(user_id, data)
+    @users ||= []
+    @last_user_id ||= 0
+
+    not_found_policy = -> { raise ActiveRecord::RecordNotFound, "Couldn't find User with 'id'=#{user_id}"}
+    user = @user.find(not_found_policy) do |u|
+      u.id == user_id
+    end
+
     data.each do |k, v|
       user[k] = v
     end
     user
+  end
+
+  def self.update_channel_users_roles(channel_id, role_set)
+    # role_set = [{user_id: 1, role_name: :operator}, ...]
+    not_found_policy = -> { raise ActiveRecord::RecordNotFound, "Couldn't find Channel with 'id'=#{channel_id}"}
+    channel = @channels.find(not_found_policy) do |c|
+      c.id == channel_id
+    end
+    @roles.keep_if {|existing_role| role_set.find{|role| existing_role.channel_id == channel.id && existing_role.user_id == role[:user_id]}}
+    role_set.each do |role|
+      set_channel_user_role(channel.id, role[:user_id], role[:role_name])
+    end
+  end
+
+  private
+
+  def self.set_channel_user_role(channel_id, user_id, role_name)
+    @roles ||= []
+    role = @roles.find{|r| r.channel_id == channel_id && r.user_id == user_id }
+    role ||= OpenStruct.new(channel_id: channel_id, user_id: user_id).tap{|r| @roles.push r }
+    role.role_name = role_name
+    role
   end
 
   def self.method_missing(name, *args, &block)
