8fcf9bc83a80956258af5aff325609d0f6673a7c
Rafael Santos <formigarafa@gmail.com>
2017-07-20 23:35:16 +1200

    mock integrations.update_user

--- test/dummy/lib/integrations.rb
+++ test/dummy/lib/integrations.rb
@@ -10,6 +10,16 @@ module Integrations
     channel
   end
 
+  def self.update_user(user_id, data)
+    @users ||= {}
+    @last_user_id ||= 0
+    user = @users[user_id] ||= OpenStruct.new(id: user_id, created_at: Time.current)
+    data.each do |k, v|
+      user[k] = v
+    end
+    user
+  end
+
   def self.method_missing(name, *args, &block)
     msg = "Fake call. Not Yet Implemented: Integrations.#{name}(#{args.inspect}) #{!!block && "{}"}"
     puts msg
