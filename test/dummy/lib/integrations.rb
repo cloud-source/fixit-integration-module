8c537dac545e19de0ea88dfbbac38b585df2f791
Rafael Santos <formigarafa@gmail.com>
2017-07-20 22:45:50 +1200

    stub integration#create_channel

--- test/dummy/lib/integrations.rb
+++ test/dummy/lib/integrations.rb
@@ -1,6 +1,15 @@
 # Stubbed Integration module.
 # This is here just a alternative implementation to mimic Thundermaps Integration module.
 module Integrations
+
+  def self.create_channel(*args)
+    @channels ||= []
+    @last_channel_id ||= 0
+    channel = OpenStruct.new(id: (@last_channel_id += 1), created_at: Time.current)
+    @channels.push channel
+    channel
+  end
+
   def self.method_missing(name, *args, &block)
     msg = "Fake call. Not Yet Implemented: Integrations.#{name}(#{args.inspect}) #{!!block && "{}"}"
     puts msg
