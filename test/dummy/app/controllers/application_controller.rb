8a395400f17718cf9ca6011cc71a3ff24165214b
Rafael Santos <formigarafa@gmail.com>
2017-07-21 00:11:50 +1200

    api to dump dummy app to check current simulated state

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def dump
+    render json: {
+      channels: Integrations.instance_eval { @channels || [] }.map(&:to_h),
+      users: Integrations.instance_eval { @users || [] }.map(&:to_h),
+      roles: Integrations.instance_eval { @roles || [] }.map(&:to_h),
+    }
+  end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'dummy/dump' => 'application#dump'
   mount SampleIntegration::Engine => "/sample_integration"
 end
